fc9fda47d10169990d87f0bde13bede1d67658ec
JangJunBeom <wkdwnsqja123@naver.com>
2024-10-18 05:46:00 +0000

    test

---
+++ chap6/prob1/main.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	struct stat buf;
+	for (i = 1; i < argc; i++) {
+		printf("%s: ", argv[i]);
+		if (lstat(argv[i], &buf) < 0) {
+				perror("lstat()");
+				continue;
+		}
+		if (S_ISREG(buf.st_mode))
+			printf("%s \n", "Regular");
+		if (S_ISDIR(buf.st_mode))
+			printf("%s \n", "Directory");
+		if (S_ISCHR(buf.st_mode))
+			printf("%s \n", "Charactor Special File");
+		if (S_ISBLK(buf.st_mode))
+			printf("%s \n", "Block Special File");
+		if (S_ISFIFO(buf.st_mode))
+			printf("%s \n", "FiFo File");
+		if (S_ISLNK(buf.st_mode))
+			printf("%s \n", "Symbolic Link");
+		if (S_ISSOCK(buf.st_mode))
+			printf("%s \n", "Socket");
+	}
+	exit(0);
+}
+		
