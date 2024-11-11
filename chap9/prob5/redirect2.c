9b0b79afbba35f72030d4bdac8de76b78e3f8d60
JangJunBeom <wkdwnsqja123@naver.com>
2024-11-11 02:09:14 +0000

    test

---
+++ chap9/prob5/redirect2.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	int child, pid, fd, status;
+	pid = fork();
+	if (pid == 0) {
+		fd = open(argv[1], O_CREAT|O_TRUNC|O_WRONLY, 0600);
+		dup2(fd, 1);
+		execvp(argv[2], &argv[2]);
+		fprintf(stderr, "%s:stderr\n", argv[1]);
+	} else {
+		child = wait(&status);
+		printf("[%d] child process %d end \n", getpid(), child);
+	}
+}
+
