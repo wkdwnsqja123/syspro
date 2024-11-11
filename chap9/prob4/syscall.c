9853b0a82017247e192efd201b8cb3f7063c8bf7
JangJunBeom <wkdwnsqja123@naver.com>
2024-11-11 02:00:57 +0000

    test

---
+++ chap9/prob4/syscall.c
@@ -0,0 +1,40 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main()
+{
+	int status;
+	if ((status = system("date")) < 0)
+		perror("system() error");
+	printf("end code %d\n", WEXITSTATUS(status));
+
+	if ((status = system("hello")) < 0)
+		perror("system() error");
+	printf("end code %d\n", WEXITSTATUS(status));
+
+	if ((status = system("who; exit 44")) < 0)
+		perror("system() error");
+	printf("end code %d\n", WEXITSTATUS(status));
+}
+int myststem (const char *cmdstring) {
+	int pid, status;
+	if (cmdstring == NULL)
+		return 1;
+	pid = fork();
+	if (pid == -1)
+		return -1;
+	if (pid == 0) {
+		execl("/bin/sh", "sh", "-c", cmdstring, (char *) 0);
+		_exit(127);
+	}
+	do {
+		if (waitpid(pid, &status, 0) == -1) {
+			if (errno != EINTR)
+				return -1;
+		} else return status;
+	} while(1);
+}
